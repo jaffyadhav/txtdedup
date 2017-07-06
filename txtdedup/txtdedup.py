c57fe7e672243063c3f4341ebe4e407eed9e3bcd
Japheth Adhavan <japheth@ideas2it.com>
2017-07-06 15:03:37 +0530

    Code Refactoring

--- txtdedup/txtdedup.py
+++ txtdedup/txtdedup.py
@@ -23,21 +23,21 @@ def _get_processed_text(text, pp_technique):
         return [stemmer.stem(w) for w in word_tokenize(text) if w not in stop_words]
 
 
-def groupdups(given_list, pp_technique='stem', similarity_ratio=70):
-    processed_list = list(map(lambda i: _get_processed_text(i, pp_technique), given_list))
+def groupdups(strings_to_group=[], pp_technique='stem', similarity_ratio=70):
+    processed_list = list(map(lambda i: _get_processed_text(i, pp_technique), strings_to_group))
     processed_indices = []
     result_list = []
-    for i, text1 in enumerate(processed_list):
-        if len(text1) > 2 and i not in processed_indices:
+    for i, string_one in enumerate(processed_list):
+        if len(string_one) > 2 and i not in processed_indices:
             processed_indices.append(i)
-            title_dups = [given_list[i]]
-            for j, text2 in enumerate(processed_list):
+            similar_strings = [strings_to_group[i]]
+            for j, string_two in enumerate(processed_list):
                 not_preprocessed = j not in processed_indices
-                is_similar = fuzz.token_set_ratio(text1, text2) >= similarity_ratio
-                if len(text2) > 2 and not_preprocessed and is_similar:
+                is_similar = fuzz.token_set_ratio(string_one, string_two) >= similarity_ratio
+                if len(string_two) > 2 and not_preprocessed and is_similar:
                     processed_indices.append(j)
-                    title_dups.append(given_list[j])
-            result_list.append(title_dups)
+                    similar_strings.append(strings_to_group[j])
+            result_list.append(similar_strings)
 
     return result_list
 
